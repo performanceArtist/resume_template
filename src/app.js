5ffad566161a93e612e37eaac856184fab1ecaa1
performanceArtist1 <nakedant19@gmail.com>
2019-05-04 15:16:03 +0600

    Add auto-import for scss, change directory structure #9 #17

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
-import './fonts/arialmt/fonts.css';
-import './fonts/arialmt_bold/fonts.css';
-import './main.scss';
+import "./fonts/arialmt/fonts.css";
+import "./fonts/arialmt_bold/fonts.css";
+
+import "./main.scss";
+
+function importAll(resolve) {
+  resolve.keys().forEach(resolve);
+}
+
+importAll(require.context("./components", true, /\.scss$/));
+importAll(require.context("./composite", true, /\.scss$/));
